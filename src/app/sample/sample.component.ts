f51490e136cd2a0591548842f981668210711e47
unknown <yesiamathira@gmail.com>
2019-03-06 15:15:01 +0530

    yyy

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -6,19 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sample.component.css']
 })
 export class SampleComponent implements OnInit {
-  buttons=[{name:"1"},{name:"2"},{name:"3"},{name:"4"},{name:"5"}];
+  button = {name:''}
+  buttons=[{name:"1"},{name:"2"},{name:"3"},{name:"4"},{name:"5"}]
   constructor() { }
 
   
   
-  ngOnInit() {
-  }
+  
   addButton(){
-    var newButton={name: "" + (this.buttons.length+1) }
+    var newButton=({name: "" + (this.buttons.length+1) });
     this.buttons.push(newButton);
   }
- removeButton(){
-   this.buttons.pop();
+//  removeButton(){
+//    this.buttons.pop();
   
- }
+//  }
+removeButton(name1){
+  console.log('button',name1)
+for(var i = 0;i<this.buttons.length;i++){
+  if(this.buttons[i].name==name1){
+    this.buttons.splice(i,1);
+  }
 }
+
+}
+ 
+ngOnInit() {
+}
+
+}
+//  {name:"1"},{name:"2"},{name:"3"},{name:"4"},{name:"5"}
